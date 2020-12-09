9a963a091152eda23995ea38a744d3bb3aceb7a1
introbeginner <intronovice@gmail.com>
2020-12-09 16:28:01 +0900

    4.7 Done

--- src/uesrs/entities/user.entity.ts
+++ src/uesrs/entities/user.entity.ts
@@ -1,6 +1,8 @@
 import { Field, InputType, ObjectType, registerEnumType } from "@nestjs/graphql";
-import { Column, Entity } from "typeorm";
+import { BeforeInsert, Column, Entity } from "typeorm";
 import { CoreEntity } from "./core.entity";
+import * as bcrypt from 'bcrypt';
+import { InternalServerErrorException } from "@nestjs/common";
 
 //type UserRole = 'client'|'owner'|'delivery';
 
@@ -28,4 +30,15 @@ export class User extends CoreEntity{
     @Column({type:"enum", enum: UserRole})  //db
     @Field(type=> UserRole)                 //graphQL
     role:UserRole;
+
+    @BeforeInsert() //when create or save ? save. create just create, not save. insert is save
+    async hashPassword(): Promise<void> {
+        try{
+            this.password = await bcrypt.hash(this.password, 10);
+        } catch(e){
+            console.log(e);
+            throw new InternalServerErrorException();
+        }
+        
+    }
 }
