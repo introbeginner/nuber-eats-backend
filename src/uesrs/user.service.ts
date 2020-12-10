a63c21ec900c0d5eadce320b4dada0bcb49b52d1
introbeginner <intronovice@gmail.com>
2020-12-10 21:33:45 +0900

    5.1

--- src/uesrs/user.service.ts
+++ src/uesrs/user.service.ts
@@ -4,11 +4,14 @@ import { Repository } from "typeorm";
 import { CreateAccountInput } from "./dtos/create-account-dto";
 import { LoginInput } from "./dtos/login.dto";
 import { User } from "./entities/user.entity";
+import * as jwt from "jsonwebtoken";
+import { ConfigService } from "@nestjs/config";
 
 @Injectable()
 export class UsersService {
     constructor(
-        @InjectRepository(User) private readonly users: Repository<User>
+        @InjectRepository(User) private readonly users: Repository<User>,
+        private readonly config: ConfigService,
     ){}
 
     async createAccount({email, password, role}: CreateAccountInput): Promise<{ok:boolean, error?:string}>{
@@ -45,6 +48,7 @@ export class UsersService {
                     error:'Wrong password'
                 }
             }
+            const token = jwt.sign({id:user.id}, this.config.get('SECRET_KEY'));
             return {
                 ok: true,
                 token: 'lalalalalalala',
