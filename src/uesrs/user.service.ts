20d57b7dd32fcce6a887dbaf76e40fd77e7279e2
introbeginner <intronovice@gmail.com>
2020-12-14 17:12:48 +0900

    6.1

--- src/uesrs/user.service.ts
+++ src/uesrs/user.service.ts
@@ -6,11 +6,13 @@ import { LoginInput } from "./dtos/login.dto";
 import { User } from "./entities/user.entity";
 import { JwtService } from "src/jwt/jwt.service";
 import { EditProfileInput } from "./dtos/edit-profile.dto";
+import { Verification } from "./entities/verification.entity";
 
 @Injectable()
 export class UsersService {
     constructor(
         @InjectRepository(User) private readonly users: Repository<User>,
+        @InjectRepository(Verification) private readonly verifications: Repository<Verification>,
         private readonly jwtService: JwtService
     ){
         
@@ -23,7 +25,12 @@ export class UsersService {
                 
                 return {ok:false, error:'There is a user with that email already'};
             }
-            await this.users.save(this.users.create({email, password, role}));
+            const user = await this.users.save(this.users.create({email, password, role}));
+            await this.verifications.save(
+                this.verifications.create({
+                    user,
+                }),
+            );
             return {ok:true};
 
         } catch(e){
@@ -72,6 +79,8 @@ export class UsersService {
 
         if(email){
             user.email = email;
+            user.verified = false;
+            await this.verifications.save(this.verifications.create({user}));
         }
         if(password){
             user.password = password;
