fe7edcb8419ca7c6bd32eb7dc24c4907ab3ad1d3
introbeginner <intronovice@gmail.com>
2020-12-18 15:39:59 +0900

    7.3

---
+++ src/uesrs/users.service.spec.ts
@@ -0,0 +1,80 @@
+import { Test } from "@nestjs/testing";
+import { getRepositoryToken } from "@nestjs/typeorm";
+import { JwtService } from "src/jwt/jwt.service";
+import { MailService } from "src/mail/mail.service";
+import { Repository } from "typeorm";
+import { User } from "./entities/user.entity";
+import { Verification } from "./entities/verification.entity";
+import { UsersService } from "./user.service";
+
+const mockRepository = {
+    findOne: jest.fn(),
+    save: jest.fn(),
+    create: jest.fn(),
+};
+
+const mockJwtService = {
+    sing: jest.fn(),
+    verify: jest.fn(),
+}
+
+const mockMailService = {
+    sendVerificationEmail: jest.fn(),
+}
+
+type MockRepository<T=any> = Partial<Record<keyof Repository<User>, jest.Mock>>;
+
+describe("UserService", () => {
+    let service: UsersService;
+    let usersRepository: MockRepository<User>;
+    
+
+    beforeAll(async () => {
+        const module = await Test.createTestingModule({
+            providers: [UsersService, {
+                provide: getRepositoryToken(User), useValue: mockRepository,
+            },
+                {
+                    provide: getRepositoryToken(Verification), useValue: mockRepository,
+                },
+                {
+                    provide: JwtService, useValue: mockJwtService,
+                },
+                {
+                    provide: MailService, useValue: mockMailService,
+                },
+            ],
+
+        }).compile();
+        service = module.get<UsersService>(UsersService);
+        usersRepository = module.get(getRepositoryToken(User));
+    });
+
+    it('be defined', () => {
+        expect(service).toBeDefined();
+    })
+
+
+    describe("createAccout", () => {
+        it("should fail if user exists", async () => {
+            usersRepository.findOne.mockResolvedValue({
+                id:1,
+                email:'lalaalalalal',
+            });
+            const result = await service.createAccount({
+                email:'',
+                password:'',
+                role:0,
+            });
+            expect(result).toMatchObject({
+                ok: false, error: 'There is a user with that email already',
+            })
+        });
+    });
+
+    it.todo('login');
+    it.todo('findById');
+    it.todo('editProfile');
+    it.todo('verifyEmail');
+
+});
